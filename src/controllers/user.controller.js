20467e2127ecd9cb6ee44071d3bd338fe9442448
Madhu25666 <madhu25666@gmail.com>
2024-06-02 16:59:17 +0530

    fixed all register controller bugs

--- src/controllers/user.controller.js
+++ src/controllers/user.controller.js
@@ -1,5 +1,6 @@
 import { asyncHandler } from "../utils/asyncHandler.js";
 import {ApiError} from "../utils/ApiError.js"
+import {ApiResponse} from "../utils/ApiResponse.js"
 import {User } from "../models/user.model.js"
 import {uploadOnCloudinary} from "../utils/cloudinary.js"
 import e from "express";
@@ -37,14 +38,17 @@ const registerUser = asyncHandler( async (req, res) => {
     }
 
     const avatarLocalPath = req.files?.avatar[0]?.path;
-    console.log("req.files: ", req.files);
 
-    const coverImageLocalPath = req.files.coverImage[0].path
+    console.log("REQ.FILES: ", req.files);
+    console.log("");
+    console.log("REQ.BODY: ", req.body);
 
-    // let coverImageLocalPath;
-    // if (req.files && Array.isArray(req.files.coverImage) && req.files.coverImage.length > 0) {
-    //     coverImageLocalPath = req.files.coverImage[0].path
-    // }
+    // const coverImageLocalPath = req.files.coverImage[0].path
+
+    let coverImageLocalPath;
+    if (req.files && Array.isArray(req.files.coverImage) && req.files.coverImage.length > 0) {
+        coverImageLocalPath = req.files.coverImage[0].path
+    }
 
     if (!avatarLocalPath) {
         throw new ApiError(400, "AvatarLocalPath is required")
